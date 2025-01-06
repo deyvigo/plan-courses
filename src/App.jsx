04ca9ed6a70ce2bd77184a3f2301dcf740725c3f
deyvigo <deyvi.gomez@unmsm.edu.pe>
2025-01-06 10:39:06 -0500

    fix: tooltip text for mobile

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ export const App = () => {
     <>
       <main className='w-[90%] h-[90%] flex justify-center items-center max-sm:flex-col max-sm:h-full max-sm:w-full'>
         <div className='relative flex-1 h-full flex justify-center items-center max-sm:w-full max-sm:h-auto'>
-          <Graph radiusCircle={1} radiusForce={1} />
+          <Graph radiusCircle={1} radiusForce={1} fontSize={'8px'}/>
         </div>
         <div className='w-[400px] h-auto p-4 flex flex-col items-center gap-8'>
           <SearchBar />
--- src/components/Tooltip.jsx
+++ src/components/Tooltip.jsx
@@ -1,4 +1,4 @@
-export const Tooltip = ({ text, x, y }) => {
+export const Tooltip = ({ text, x, y, fontSize }) => {
   return (
     <div
       style={{
@@ -9,7 +9,7 @@ export const Tooltip = ({ text, x, y }) => {
         boxShadow: '0px 2px 5px rgba(0, 0, 0, 0.3)',
         pointerEvents: 'none',
         color: 'black',
-        fontSize: '0.8em',
+        fontSize: fontSize,
         left: `${x + Math.floor(Math.random() * (30 - 10 + 1)) + 20}px`,
         top: `${y - Math.floor(Math.random() * (40 - 10 + 1)) + 20}px`,
       }}
